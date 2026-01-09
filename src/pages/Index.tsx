919272f763db3b67fb390dcaa57c36be7ecef581
ADARSH ALEX BALMUCHU <adarshalex.balmuchui23@iimranchi.ac.in>
2026-01-09 20:45:40 +0000

    feat: Remove Why Choose Us, Promo Banner, and Newsletter sections for cleaner layout

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,6 @@
 import Header from "@/components/Header";
 import HeroBanner from "@/components/HeroBanner";
 import ProductSection from "@/components/ProductSection";
-import WhyChooseUs from "@/components/WhyChooseUs";
-import PromoBanner from "@/components/PromoBanner";
-import Newsletter from "@/components/Newsletter";
 import Footer from "@/components/Footer";
 
 const Index = () => {
@@ -13,10 +10,7 @@ const Index = () => {
       <main>
         <HeroBanner />
         <ProductSection />
-        <WhyChooseUs />
-        <PromoBanner />
       </main>
-      <Newsletter />
       <Footer />
     </div>
   );
